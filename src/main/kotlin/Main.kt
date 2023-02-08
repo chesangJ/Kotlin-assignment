e0e3211049f9eca76d65f6db41dc90b551e6232a
Mitingi Joy <mitingijoychesang@gmail.com>
2023-02-08 21:37:26 +0300

    used different operations

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(){
+    printname()
+    add()
+    modules()
+    println("Hello world")
+}
+fun modules(){
+    var x = 55
+    var y = 10
+     x = x % y
+    println(x)
+}
+fun add(){
+    var x = 40
+    var y = 30
+    x = x+y
+    println(x)
+}
+
+fun printname(){
+    println( "I love eating fries")
+}
